8374f733e313bab4aa0acf6d3ba5436aa2de21a6
DESKTOP-S77QMIR\vive_1 <1500825146@qq.com>
2019-01-29 17:33:58 +0800

    Mesh_Player_0129

---
+++ Assets/OBJImport/Samples/FileControllor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class FileControllor : MonoBehaviour
+{
+    static public String Pathavgr = null;
+    public void OpenProject()
+    {
+        OpenDialogDir ofn2 = new OpenDialogDir();
+        ofn2.pszDisplayName = new String(new char[2000]); ;     // 存放目录路径缓冲区  
+        ofn2.lpszTitle = "Select Folder";// 标题  
+        //ofn2.ulFlags = BIF_NEWDIALOGSTYLE | BIF_EDITBOX; // 新的样式,带编辑框  
+        IntPtr pidlPtr = DllOpenFileDialog.SHBrowseForFolder(ofn2);
+
+        char[] charArray = new char[2000];
+        for (int i = 0; i < 2000; i++)
+            charArray[i] = '\0';
+
+        DllOpenFileDialog.SHGetPathFromIDList(pidlPtr, charArray);
+        Pathavgr = new String(charArray);
+        Pathavgr = Pathavgr.Substring(0, Pathavgr.IndexOf('\0'));
+
+        Debug.Log(Pathavgr);//这个就是选择的目录路径
+    }
+    public void SaveProject()
+    {
+        
+    }
+
+}
---
+++ Assets/OBJImport/Samples/ObjFromFile.cs
@@ -0,0 +1,63 @@
+﻿using Dummiesman;
+using System;
+using System.IO;
+using UnityEngine;
+public class ObjFromFile : MonoBehaviour
+{
+    public string Path = null;
+    public string[] Files;
+    public string[] Mtls;
+    public OBJLoader loadedObj = new OBJLoader();
+    public OBJLoader loadedObj1 = new OBJLoader();
+    public int i=0;
+    public GameObject temp;
+    public GameObject temp1;
+    public bool Flags = false;
+    void Start()
+    {
+        temp = new GameObject();
+        //System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        //sw.Start();
+        //temp = loadedObj.Load(Files[0], Mtls[0]);
+        //sw.Stop();
+        //TimeSpan ts2 = sw.Elapsed;
+        //Debug.Log(ts2.TotalSeconds);
+        //temp.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+    }
+
+
+    void Update()
+    {
+        if (!string.IsNullOrEmpty(FileControllor.Pathavgr))
+        {
+            Path = FileControllor.Pathavgr;
+            Files = Directory.GetFiles(Path, "*.obj");
+            Mtls = Directory.GetFiles(Path, "*.mtl");
+            Flags = true;
+        }
+
+        if (Flags)
+        {
+            if (i < Files.Length && i > 0)
+            {
+                Destroy(temp);
+                Resources.UnloadUnusedAssets();
+                temp = null;
+                temp = loadedObj.Load(Files[i], Mtls[i]);
+                temp.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+            }
+            else if (i == 0)
+            {
+                temp = loadedObj.Load(Files[i], Mtls[i]);
+                temp.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            }
+            else if (i == Files.Length)
+            {
+                i = 0;
+            }
+            i++;
+        }
+    }
+    
+}
